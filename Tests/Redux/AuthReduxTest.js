794d67e34d03bbc0aa193e9e229a8ef2e385ebf1
Martin Stabenfeldt <martin.stabenfeldt@shortcut.no>
2017-10-24 11:57:33 +0200

    Update test

--- Tests/Redux/AuthReduxTest.js
+++ Tests/Redux/AuthReduxTest.js
@@ -1,27 +1,27 @@
-import Actions, { reducer, INITIAL_STATE } from '../../App/Redux/GithubRedux'
+import Actions, { reducer, INITIAL_STATE } from '../../App/Redux/AuthRedux'
 
 test('request', () => {
   const username = 'taco'
-  const state = reducer(INITIAL_STATE, Actions.userRequest(username))
+  const state = reducer(INITIAL_STATE, Actions.tokenRequest(username))
 
   expect(state.fetching).toBe(true)
   expect(state.username).toBe(username)
-  expect(state.avatar).toBeNull()
+  expect(state.token).toBeNull()
 })
 
 test('success', () => {
-  const avatar = 'http://placekitten.com/200/300'
-  const state = reducer(INITIAL_STATE, Actions.userSuccess(avatar))
+  const token = 'https://gist.github.com/stabenfeldt/14db8a93d8bffd5258e29c03e2dfe234'
+  const state = reducer(INITIAL_STATE, Actions.tokenSuccess(token))
 
   expect(state.fetching).toBe(false)
-  expect(state.avatar).toBe(avatar)
+  expect(state.token).toBe(token)
   expect(state.error).toBeNull()
 })
 
 test('failure', () => {
-  const state = reducer(INITIAL_STATE, Actions.userFailure())
+  const state = reducer(INITIAL_STATE, Actions.tokenFailure())
 
   expect(state.fetching).toBe(false)
   expect(state.error).toBe(true)
-  expect(state.avatar).toBeNull()
+  expect(state.token).toBeNull()
 })
