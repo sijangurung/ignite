0f28dd11514315fd86ea7e53b58a9a84c2198104
Martin Stabenfeldt <martin.stabenfeldt@shortcut.no>
2017-10-25 14:17:53 +0200

    cleanup

--- App/Services/Api.js
+++ App/Services/Api.js
@@ -1,69 +1,14 @@
-// a library to wrap and simplify api calls
-import apisauce from 'apisauce'
+import {create} from 'apisauce'
 
-// our "constructor"
-const create = (baseURL = 'https://api.github.com/') => {
-  // ------
-  // STEP 1
-  // ------
-  //
-  // Create and configure an apisauce-based api object.
-  //
-  const api = apisauce.create({
-    // base URL is read from the "constructor"
-    baseURL,
-    // here are some default headers
-    headers: {
-      'Cache-Control': 'no-cache'
-    },
-    // 10 second timeout...
-    timeout: 10000
-  })
+const api = create({
+  baseURL: 'https://gist.githubusercontent.com',
+})
 
-  // ------
-  // STEP 2
-  // ------
-  //
-  // Define some functions that call the api.  The goal is to provide
-  // a thin wrapper of the api layer providing nicer feeling functions
-  // rather than "get", "post" and friends.
-  //
-  // I generally don't like wrapping the output at this level because
-  // sometimes specific actions need to be take on `403` or `401`, etc.
-  //
-  // Since we can't hide from that, we embrace it by getting out of the
-  // way at this level.
-  //
-  const getRoot = () => api.get('')
-  const getRate = () => api.get('rate_limit')
-  const getUser = (username) => api.get('search/users', {q: username})
+export default api
+  .get('/stabenfeldt/14db8a93d8bffd5258e29c03e2dfe234/raw/207c7facfa8ad5c1cce20ed08d954dad4a8ee75e/token.json')
+  .then((response) => response.data)
 
-  // Nothing done with the username provided here, yet.
-  const getUserToken = (username) => api.get('https://gist.githubusercontent.com/stabenfeldt/'+
-        '14db8a93d8bffd5258e29c03e2dfe234/raw/207c7facfa8ad5c1cce20ed08d954dad4a8ee75e/token.json')
+/* The result we're looking for is
+  { "token": "secret-sauce" }
+*/
 
-  // ------
-  // STEP 3
-  // ------
-  //
-  // Return back a collection of functions that we would consider our
-  // interface.  Most of the time it'll be just the list of all the
-  // methods in step 2.
-  //
-  // Notice we're not returning back the `api` created in step 1?  That's
-  // because it is scoped privately.  This is one way to create truly
-  // private scoped goodies in JavaScript.
-  //
-  return {
-    // a list of the API functions from step 2
-    getUserToken,
-    getRoot,
-    getRate,
-    getUser
-  }
-}
-
-// let's return back our create method as the default.
-export default {
-  create
-}
